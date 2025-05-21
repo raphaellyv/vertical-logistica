6864c7f06fb72fd4a8f9193bbb0068ad5379481e
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-05-21 10:27:18 -0300

    Add user to orders and adjust total comparison validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
   validates :order_id, :total, :date, presence: true
   validates :order_id, uniqueness: true
-  validates :total, comparison: { greater_than: 0 }
+  validates :total, comparison: { greater_than_or_equal_to: 0 }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -30,7 +30,8 @@ RSpec.describe Order, type: :model do
 
     context 'uniqueness' do
       it 'requires order_id to be unique' do
-        Order.create(order_id: 1, total: 20.2, date: Date.current)
+        user = User.create!(user_id: 1, name: 'Souza')
+        Order.create!(order_id: 1, total: 20.2, date: Date.current, user:)
         order = Order.new(order_id: 1)
 
         order.valid?
@@ -40,15 +41,18 @@ RSpec.describe Order, type: :model do
     end
 
     context 'comparison' do
-      it 'requires total to be greater than 0' do
+      it 'requires total to be greater than or equal to 0' do
         order1 = Order.new(total: 0)
         order2 = Order.new(total: 0.01)
+        order3 = Order.new(total: -0.01)
 
         order1.valid?
         order2.valid?
+        order3.valid?
 
-        expect(order1.errors.include?(:total)).to be true
+        expect(order1.errors.include?(:total)).to be false
         expect(order2.errors.include?(:total)).to be false
+        expect(order3.errors.include?(:total)).to be true
       end
     end
   end
