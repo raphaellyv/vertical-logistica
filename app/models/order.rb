a75f68b290245af4f6dd15ae796ea2b32f8a8845
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-05-21 13:47:36 -0300

    Add update_total method to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :products
 
   validates :order_id, :total, :date, presence: true
   validates :order_id, uniqueness: true
   validates :total, comparison: { greater_than_or_equal_to: 0 }
+
+  def update_total
+    self.total = products.sum(:value)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -65,4 +65,26 @@ RSpec.describe Order, type: :model do
       expect(order.total).to eq 0
     end
   end
+
+  describe '#update_total' do
+    it 'updates the total price of an order' do
+      user1 = User.create!(user_id: 1, name: 'Joaquim')
+      user2 = User.create!(user_id: 2, name: 'Pietro')
+
+      order1 = Order.create!(order_id: 1, date: Date.new(2020, 1, 1), user: user1)
+      order2 = Order.create!(order_id: 2, date: Date.new(2020, 2, 2), user: user1)
+      order3 = Order.create!(order_id: 3, date: Date.new(2020, 3, 3), user: user2)
+      
+      expect(order1.total).to eq 0
+
+      Product.create!(product_id: 1, value: 2.99, order: order1)
+      Product.create!(product_id: 2, value: 5.0, order: order1)
+      Product.create!(product_id: 3, value: 10.0, order: order2)
+      Product.create!(product_id: 3, value: 20.0, order: order3)
+
+      order1.update_total
+
+      expect(order1.total).to eq 7.99
+    end
+  end
 end
