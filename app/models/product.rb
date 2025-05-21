0389d61ddd8b4d0552868c5e561e891e590a114b
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-05-21 13:50:32 -0300

    Add after_save callback to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :order
+  after_save :update_order_total
 
   validates :product_id, :value, presence: true
   validates :value, comparison: { greater_than: 0 }
+
+  private
+    def update_order_total
+      order.update_total
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -33,4 +33,51 @@ RSpec.describe Product, type: :model do
       end
     end
   end
+
+  describe 'on create' do
+    it 'updates product order total' do
+      user1 = User.create!(user_id: 1, name: 'Joaquim')
+      user2 = User.create!(user_id: 2, name: 'Pietro')
+
+      order1 = Order.create!(order_id: 1, date: Date.new(2020, 1, 1), user: user1)
+      order2 = Order.create!(order_id: 2, date: Date.new(2020, 2, 2), user: user1)
+      order3 = Order.create!(order_id: 3, date: Date.new(2020, 3, 3), user: user2)
+
+      product1 = Product.new(product_id: 1, value: 2.99, order: order1)
+      product2 = Product.new(product_id: 2, value: 5.0, order: order1)
+      product3 = Product.new(product_id: 3, value: 10.0, order: order2)
+      product4 = Product.new(product_id: 3, value: 20.0, order: order3)
+
+      expect(order1.total).to eq 0
+
+      product1.save!
+      product2.save!
+      product3.save!
+      product4.save!
+
+      expect(order1.total).to eq 7.99
+    end
+  end
+
+  describe 'on update' do
+    it 'updates product order total' do
+      user1 = User.create!(user_id: 1, name: 'Joaquim')
+      user2 = User.create!(user_id: 2, name: 'Pietro')
+
+      order1 = Order.create!(order_id: 1, date: Date.new(2020, 1, 1), user: user1)
+      order2 = Order.create!(order_id: 2, date: Date.new(2020, 2, 2), user: user1)
+      order3 = Order.create!(order_id: 3, date: Date.new(2020, 3, 3), user: user2)
+
+      product1 = Product.create!(product_id: 1, value: 2.99, order: order1)
+      product2 = Product.create!(product_id: 2, value: 5.0, order: order1)
+      product3 = Product.create!(product_id: 3, value: 10.0, order: order2)
+      product4 = Product.create!(product_id: 3, value: 20.0, order: order3)
+
+      expect(order1.total).to eq 7.99
+
+      product1.update!(value: 3.99)
+
+      expect(order1.total).to eq 8.99
+    end
+  end
 end
