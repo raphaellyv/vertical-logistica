d774a529f0341a885cb54016419ab2f649aaaa77
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-05-24 20:56:38 -0300

    Move filters from user_orders_serializer to orders_list_serializer

--- app/serializers/orders_list_serializer.rb
+++ app/serializers/orders_list_serializer.rb
@@ -1,7 +1,19 @@
 class OrdersListSerializer
   def self.create_serialized_hash_array(filters: {})
-    User.order(:user_id)
-        .map{ |user| UserOrdersSerializer.create_serialized_hash(user:, filters:) }
+    orders = Order.all
+
+    if filters.values.any?
+      filters.each do |key, value|
+        orders =  orders.public_send("filter_by_#{key}", value) if value.present?
+      end
+    end
+    
+    grouped_orders = orders.order(:order_id).group_by(&:user_id)
+    filtered_user_ids = grouped_orders.keys
+
+    User.where(user_id: filtered_user_ids)
+        .order(:user_id)
+        .map{ |user| UserOrdersSerializer.create_serialized_hash(user: user, orders: grouped_orders[user.user_id]) }
         .select{ |user| user['orders'].any? }
   end
 end
--- app/serializers/user_orders_serializer.rb
+++ app/serializers/user_orders_serializer.rb
@@ -1,20 +1,9 @@
 class UserOrdersSerializer
-  def self.create_serialized_hash(user:, filters: {})
-    @filters = filters
-    @user_orders = user.orders.order(:order_id)
+  def self.create_serialized_hash(user:, orders:)
     user_hash = user.serializable_hash(only: %i[user_id name])
     
-    filter_user_orders if filters.values.any?
-    
-    user_hash['orders'] = @user_orders.map{ |order| OrderSerializer.create_serialized_hash(order)}
+    user_hash['orders'] = orders.sort_by(&:order_id)
+                                .map{ |order| OrderSerializer.create_serialized_hash(order)}
     user_hash
   end 
-
-  private
-
-  def self.filter_user_orders
-    @filters.each do |key, value|
-      @user_orders =  @user_orders.public_send("filter_by_#{key}", value) if value.present?
-    end
-  end
 end
